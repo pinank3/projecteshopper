19cb556b3dc29db578d23909324e05c3c4b901d6
pinank3 <37895606+pinank3@users.noreply.github.com>
2018-05-10 14:19:12 +0530

    Add files via upload

--- project/assets/database.sql
+++ project/assets/database.sql
@@ -23,3 +23,12 @@ alter table products add column pro_path text after pro_brid;
 insert into products values ('','mens office formal wear',2500.00,20,'mens office formal wear',1,3,'product1.jpg','');
 insert into products values ('','Womens Party wear',2000.00,40,'Womens Party wear',2,2,'product6.jpg','');
 insert into products values ('','Kids Tshirt wear',1500.00,80,'Kids Tshirt wear',3,4,'product11.jpg','');
+
+create table users(
+	us_id int auto_increment primary key,
+	us_name varchar(100),
+	us_mobile bigint(100),
+	us_email varchar(100),
+	us_password varchar(100),
+	us_time timestamp
+);
