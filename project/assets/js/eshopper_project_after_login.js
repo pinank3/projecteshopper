26a12fca18b963554a7d8b18ea02162b76e708fb
pinank3 <37895606+pinank3@users.noreply.github.com>
2018-05-17 13:45:56 +0530

    Add files via upload

--- project/assets/js/eshopper_project_after_login.js
+++ project/assets/js/eshopper_project_after_login.js
@@ -5,4 +5,18 @@ $(document).ready(function(){
 			$(".msg_update").html(res);
 		})
 	})
+	$(".btn-brand").click(function(){
+		// alert(1)
+		records =$("#brand_form").serialize();
+		$.post("../controllers/brand-action.php",records).success(function(res){
+			$(".msg_brand").html(res);
+		})
+	})
+	$(".btn-category").click(function(){
+		// alert(1)
+		records =$("#category_form").serialize();
+		$.post("../controllers/category-action.php",records).success(function(res){
+			$(".msg_category").html(res);
+		})
+	})
 })
